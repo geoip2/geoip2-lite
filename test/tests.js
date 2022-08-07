bded16c0c7794a3cdd12dee4c0a9ab3bc094fe06
unknown <chandragupta@kreationnext.com>
2022-08-07 13:15:41 +0530

    Minor bug fixes

--- test/tests.js
+++ test/tests.js
@@ -1,80 +1,52 @@
 var geoip = require('../lib/geoip2');
 
 module.exports = {
-	testLookup: function(test) {
+	testLookup: function (test) {
 		test.expect(2);
 
 		var ip = '8.8.4.4';
 		var ipv6 = '2001:4860:b002::68';
-
 		var actual = geoip.lookup(ip);
-
 		test.ok(actual, 'should return data about IPv4.');
-
 		actual = geoip.lookup(ipv6);
-
 		test.ok(actual, 'should return data about IPv6.');
-
 		test.done();
 	},
-    
-	testDataIP4: function(test) {
-		test.expect(9);
 
+	testDataIP4: function (test) {
+		test.expect(9);
 		var ip = '72.229.28.185';
-
 		var actual = geoip.lookup(ip);
-
 		test.notStrictEqual(actual.range, undefined, 'should contain IPv4 range');
-        
 		test.strictEqual(actual.country, 'US', "should match country");
-        
 		test.strictEqual(actual.region, 'NY', "should match region");
-        
 		test.strictEqual(actual.eu, '0', "should match eu");
-        
 		test.strictEqual(actual.timezone, 'America/New_York', "should match timezone");
-        
 		test.strictEqual(actual.city, 'New York', "should match city");
-        
 		test.ok(actual.ll, 'should contain coordinates');
-        
+
 		test.strictEqual(actual.metro, 501, "should match metro");
-        
 		test.strictEqual(actual.area, 1, "should match area");
-
 		test.done();
 	},
-    
-	testDataIP6: function(test) {
+
+	testDataIP6: function (test) {
 		test.expect(9);
-
 		var ipv6 = '2001:1c04:400::1';
-
 		var actual = geoip.lookup(ipv6);
-
 		test.notStrictEqual(actual.range, undefined, 'should contain IPv6 range');
-        
 		test.strictEqual(actual.country, 'NL', "should match country");
-        
 		test.strictEqual(actual.region, 'NH', "should match region");
-        
 		test.strictEqual(actual.eu, '1', "should match eu");
-        
 		test.strictEqual(actual.timezone, 'Europe/Amsterdam', "should match timezone");
-        
 		test.strictEqual(actual.city, 'Amsterdam', "should match city");
-        
 		test.ok(actual.ll, 'should contain coordinates');
-        
 		test.strictEqual(actual.metro, 0, "should match metro");
-        
 		test.strictEqual(actual.area, 5, "should match area");
-
 		test.done();
 	},
 
-	testUTF8: function(test) {
+	testUTF8: function (test) {
 		test.expect(2);
 
 		var ip = "2.139.175.1";
@@ -87,14 +59,12 @@ module.exports = {
 		test.done();
 	},
 
-	testMetro: function(test) {
+	testMetro: function (test) {
 		test.expect(2);
 
 		var actual = geoip.lookup("23.240.63.68");
-
 		test.equal(actual.city, "Riverside"); //keeps changing with each update from one city to other (close to each other geographically)
 		test.equal(actual.metro, 803);
-
 		test.done();
 	},
 
@@ -108,29 +78,29 @@ module.exports = {
 
 		test.done();
 	},
-    
+
 	testSyncReload: function (test) {
 		test.expect(6);
 
 		//get original data
 		var before4 = geoip.lookup("75.82.117.180");
 		test.notEqual(before4, null);
-        
+
 		var before6 = geoip.lookup("::ffff:173.185.182.82");
 		test.notEqual(before6, null);
-        
+
 		//clear data;
 		geoip.clear();
-        
+
 		//make sure data is cleared
 		var none4 = geoip.lookup("75.82.117.180");
 		test.equal(none4, null);
 		var none6 = geoip.lookup("::ffff:173.185.182.82");
 		test.equal(none6, null);
-        
+
 		//reload data synchronized
 		geoip.reloadDataSync();
-        
+
 		//make sure we have value from before
 		var after4 = geoip.lookup("75.82.117.180");
 		test.deepEqual(before4, after4);
@@ -139,7 +109,7 @@ module.exports = {
 
 		test.done();
 	},
-    
+
 	testAsyncReload: function (test) {
 		test.expect(6);
 
@@ -148,25 +118,25 @@ module.exports = {
 		test.notEqual(before4, null);
 		var before6 = geoip.lookup("::ffff:173.185.182.82");
 		test.notEqual(before6, null);
-        
+
 		//clear data;
 		geoip.clear();
-        
+
 		//make sure data is cleared
 		var none4 = geoip.lookup("75.82.117.180");
 		test.equal(none4, null);
 		var none6 = geoip.lookup("::ffff:173.185.182.82");
 		test.equal(none6, null);
-        
+
 		//reload data asynchronously
-		geoip.reloadData(function(){
+		geoip.reloadData(function () {
 			//make sure we have value from before
 			var after4 = geoip.lookup("75.82.117.180");
 			test.deepEqual(before4, after4);
 			var after6 = geoip.lookup("::ffff:173.185.182.82");
 			test.deepEqual(before6, after6);
 
-			test.done(); 
+			test.done();
 		});
 	}
 };
